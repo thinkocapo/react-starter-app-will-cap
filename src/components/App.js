9a5ab59e0b7f7ff1fa5032838ac0c0be0d0438d3
thinkocapo <thinkocapo@gmail.com>
2018-01-22 11:18:19 -0500

    redux implementation started in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import { Button } from 'react-bootstrap'
 
-const App = ({ message = 'Hello Starter App!' }) => (
-  <Button bsStyle='primary' bsSize='large'>{message}</Button>
-)
+// const App = ({ message = 'Hello Starter App!' }) => (
+//   <Button bsStyle='primary' bsSize='large'>{message}</Button>
+// )
 
-App.propTypes = {
-  message: React.PropTypes.string
+// App.propTypes = {
+//   message: React.PropTypes.string
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <Button bsStyle='primary' bsSize='large'>Will-Cap Starter App</Button>
+    )
+  }
 }
-
 export default App
+
+// function mapStateToProps(state, ownProps) {
+//   return {
+
+//   }
+// }
+// const actions = {}
+// export default connect(mapStateToProps, actions)(App)
